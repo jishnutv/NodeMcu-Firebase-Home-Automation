51ac850e5b34e7454f69776cf49558cecbb5256e
Jishnu T V <jishnuprgm@gmail.com>
2019-11-25 18:04:29 +0530

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+# NodeMcu-Firebase-Home-Automation
+## This is a simple home automation project using NodeMcu and Firebase
+
+ 1. Download Zip
+ 2. Extract and open the .ino file with arduino IDE
+ 3. Add your wifi ssid, password and Firebase host, auth
+ 4. Upload to NodeMcu
+ 5. Open public fonder inside Automation_App
+ 6. Open js folder and edit config.js file
+ 7. Open index.html with browser
